05785ec54b0c7cbf98dc362b0e351383844523fe
FelipeRojasC <103686460+FelipeRojasC@users.noreply.github.com>
2024-10-03 16:39:19 -0300

    feat: Add migration in project

--- Program.cs
+++ Program.cs
@@ -1,9 +1,19 @@
+
+using DotNetEnv;
+using Microsoft.EntityFrameworkCore;
+using Taller1_WebMovil.Src.Data;
+
+Env.Load();
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+string connectionString = Environment.GetEnvironmentVariable("DATABASE_URL") ?? "Data Source = app.db";
+
+builder.Services.AddDbContext<ApplicationDbContext>(opt =>opt.UseSqlite(connectionString));
 
 var app = builder.Build();
 
---
+++ Src/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Taller1_WebMovil.Src.Models;
+
+namespace Taller1_WebMovil.Src.Data
+{
+    public class ApplicationDbContext(DbContextOptions dbContextOptions) : DbContext(dbContextOptions)
+    {
+        public DbSet<User> Users {get; set;} = null!;
+        public DbSet<Product> Products {get; set;} = null!;
+        public DbSet<Purchase> Purchases {get; set;} = null!;
+        public DbSet<Role> Roles {get; set;} = null!;
+        public DbSet<Gender> Genders {get; set;} = null!;
+        public DbSet<Category> Categories {get; set;} = null!;
+        public DbSet<PurchaseReceipt> PurchaseReceipts {get; set;} = null!;
+    }
+}
