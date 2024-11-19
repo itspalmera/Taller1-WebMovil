71ffbd11489989f2c70e2ac216c48ef288005909
FelipeRojasC <103686460+FelipeRojasC@users.noreply.github.com>
2024-11-19 14:35:48 -0300

    feat: add initial users and roles to data seeders

--- Src/Data/ApplicationDbContext.cs
+++ Src/Data/ApplicationDbContext.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Taller1_WebMovil.Src.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace Taller1_WebMovil.Src.Data
 {
@@ -12,8 +14,24 @@ namespace Taller1_WebMovil.Src.Data
     {
         public ApplicationDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
     {
-        
+    
     }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            List<IdentityRole> roles = new List<IdentityRole>
+            {
+                new IdentityRole {
+                    Id="1",Name="Administrador",NormalizedName="ADMINISTRADOR"
+                },
+                new IdentityRole {
+                    Id="2",Name="Cliente",NormalizedName="CLIENTE"
+                }
+            };
+            modelBuilder.Entity<IdentityRole>().HasData(roles);
+    
+        }
         public DbSet<Product> Products {get; set;} = null!;
         public DbSet<Purchase> Purchases {get; set;} = null!;
         public DbSet<Gender> Genders {get; set;} = null!;
--- Src/Data/DataSeeders.cs
+++ Src/Data/DataSeeders.cs
@@ -2,16 +2,19 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Taller1_WebMovil.Src.Models;
 
 namespace Taller1_WebMovil.Src.Data
 {
     public class DataSeeders
     {
-        public static void Iniialize(IServiceProvider serviceProvider){
+        public static async void Iniialize(IServiceProvider serviceProvider){
             using (var scope = serviceProvider.CreateScope()){
                 var services = scope.ServiceProvider;
                 var context = services.GetRequiredService<ApplicationDbContext>();
+                var userManager = services.GetRequiredService<UserManager<User>>();  // Obtén el UserManager
+                var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();  // Obtén el RoleManager
 
                if(!context.Genders.Any()){
                     context.Genders.AddRange(
@@ -32,20 +35,34 @@ namespace Taller1_WebMovil.Src.Data
                 }
                 context.SaveChanges();
 
-                /*if(!context.Users.Any())
+                if(!context.Users.Any())
                 {
                     var user = new User { 
                         rut = "20.416.699-4", 
                         name = "Ignacio Mancilla",
                         birthDate = new DateOnly(2000,10,25), 
-                        email = "admin@idwm.cl.",
+                        Email = "admin@idwm.cl.",
+                        UserName="admin@idwm.cl.",
                         genderId = 1,
                         enable = true,
                     };
-                    context.Users.Add(user);
+                    //Asignamos la contraseña al usuario.
+                    var result = await userManager.CreateAsync(user, "P4ssw0rd!");
+                    if (result.Succeeded)
+                    {
+                        // Asignamos el rol de "Administrador".
+                        await userManager.AddToRoleAsync(user, "Administrador");
+                    }
+                    else
+                    {
+                        foreach (var error in result.Errors)
+                        {
+                            Console.WriteLine(error.Description);
+                        }
+                        }
                 }
-                context.SaveChanges();
-                */
+                //Se guardan los cambios
+                await serviceProvider.GetRequiredService<ApplicationDbContext>().SaveChangesAsync();
             }
             
         }
